9efd06f2d0dda8229f85a9b98301334e7ad8a569
Flochabs <61731509+Flochabs@users.noreply.github.com>
2020-06-10 09:15:31 +0200

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/MS5/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/MS5/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\MS5\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\MS5\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
