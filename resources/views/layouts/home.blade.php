020b7fe5451250cf980f33edd0bf3e676a0f9aa9
Flochabs <61731509+Flochabs@users.noreply.github.com>
2020-06-12 11:57:59 +0200

    mise en place des routes boutton home page

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+
+</head>
+<body>
+
+<div id="app">
+    @if(Request::is('/'))
+        <header class="container-fluid p-0">
+            <div class="row no-gutters">
+                <div class="col-md-12">
+                    <div class="banner justify-content-center">
+                        <div class="container">
+                            <div class="row no-gutters justify-content-end pt-5">
+                                <i class="fab fa-instagram fa-2x icon text-center pt-1 m-2"></i>
+                                <i class="fab fa-facebook fa-2x icon text-center pt-1 m-2"></i>
+                                <i class="fab fa-twitter fa-2x icon text-center pt-1 m-2"></i>
+                            </div>
+                            <div class="row no-gutters justify-content-center pt-4">
+                                <img class="img-fluid mt-5" src="{{asset('storage/images/Logo.png')}}" alt="logo">
+                            </div>
+
+                            @guest
+                                <div class="row no-gutters justify-content-center p-5">
+                                    <li class="nav-item">
+                                        <a class="bouton-connexion"
+                                           href="{{ route('login') }}">{{ __('Connexion') }}</a>
+                                    </li>
+                                    @if (Route::has('register'))
+                                        <li class="nav-item">
+                                            <a class="bouton-inscription"
+                                               href="{{ route('register') }}">{{ __('Inscription') }}</a>
+                                        </li>
+                                    @endif
+
+                                    @endguest
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </header>
+    @endif
+
+    <main class="">
+        @yield('content')
+    </main>
+</div>
+</body>
+</html>
