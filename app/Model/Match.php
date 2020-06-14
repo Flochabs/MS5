ed9af862cffb9fa352745867018c61b42ee9813f
jeremy <royjerem@gmail.com>
2020-06-14 21:48:48 +0200

    création de deux méthode homeTeamName et awayTeamName pour récuperer les noms des teams

--- app/Model/Match.php
+++ app/Model/Match.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Match extends Model
 {
     //
+    protected $table = 'matchs';
+
+    public function homeTeamName() {
+        return $this->belongsTo(Team::class, 'home_team_id');
+    }
+
+    public function awayTeamName() {
+        return $this->belongsTo(Team::class, 'away_team_id');
+    }
+
 }
