1ed4157c8945752bd3067ae9d6364febe6ea0b5e
jeremy <royjerem@gmail.com>
2020-06-15 22:19:27 +0200

    récupération des données de l'users grâce à la variable

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -2,13 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Match;
+use App\Model\Player;
+use App\Model\Team;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class MatchController extends Controller
 {
     //
     public function index()
     {
+        //-------------  RECUPERATION DONNES UTILISATEUR ---------------//
+        //récupération des données users
+        $user = Auth::user();
+        //dd($user);
+
+
+
+
         return view('match.index');
     }
 }
