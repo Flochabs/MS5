f42ebbc01b3180f5bc94d52ca25ec0b9ac3d22e7
jeremy <royjerem@gmail.com>
2020-06-15 16:29:44 +0200

    ajout des commentaires sur les méthodes getplayers et getleague

--- app/Model/Team.php
+++ app/Model/Team.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    // Recupérer les joueurs qui appartiennent une team
     public function getPlayers() {
         return $this->belongsToMany(Player::class);
     }
 
+    // Recupérer la league qui appartiennent à seul team
     public function getLeague() {
         return $this->belongsTo(League::class, 'league_id');
     }
