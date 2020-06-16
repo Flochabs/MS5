6dafe5cb89582d84dc7db51bb03658070cca4e2e
jeremy <royjerem@gmail.com>
2020-06-16 10:36:26 +0200

    commentaire sur les méthodes hometeamName et awayTeamName

--- app/Model/Match.php
+++ app/Model/Match.php
@@ -9,12 +9,16 @@ class Match extends Model
     //
     protected $table = 'matchs';
 
+    // Récupére le nom des équipes présent dans le match grâce à la clé étranger home_team_id dans la table match
     public function homeTeamName() {
         return $this->belongsTo(Team::class, 'home_team_id');
     }
 
+
+    // Récupére le nom des équipes présent dans le match grâce à la clé étranger away_team_id dans la table match
     public function awayTeamName() {
         return $this->belongsTo(Team::class, 'away_team_id');
     }
+
 
 }
