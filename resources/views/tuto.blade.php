16af8e8d1a499566090aed49b505919f58ad9bb9
ArmelBouvier <60919177+ArmelBouvier@users.noreply.github.com>
2020-06-18 10:08:52 +0200

    page tuto créée, stylisée, manque l'image coaching

--- resources/views/tuto.blade.php
+++ resources/views/tuto.blade.php
@@ -53,6 +53,7 @@
             </div>
         </div>
 
+        {{--Première section tuto : leagues--}}
         <div class="container my-5">
             <div class="row my-5">
                 <div class="col-md-6" id="first-step"></div>
@@ -79,6 +80,8 @@
             </div>
         </div>
 
+
+        {{--        Deuxième section tuto : teams --}}
         <div class="container my-5">
             <div class="row my-5">
                 <div class="col-md-6 card shadow-lg">
@@ -99,4 +102,70 @@
             </div>
         </div>
     </div>
+
+    {{--        Troisième section tuto : draft --}}
+    <div class="container-fluid mt-5 py-5" id="second-banner">
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-md-6" id="third-step"></div>
+                <div class="col-md-6 card shadow-lg">
+                    <div class="py-4 d-flex flex-column align-items-center">
+                        <h1 class="display-2 text-dark">3rd Step</h1>
+                        <img src="{{asset('storage/images/picto_joueur.png')}}" alt="photo">
+                        <h2>Fais ta draft</h2>
+                    </div>
+                    <div class="card-body mt-1">
+                        <p class="card-text mb-1">Maintenant, on entre dans le vif du sujet !</p>
+                        <p class="card-text mb-1">Tu vas pouvoir batailler avec tes adversaires
+                            pour choisir les joueurs NBA qui te plaisent.</p>
+                        <p class="card-text mb-1">Tu as un salary cap de <span class="font-weight-bold tertiary">200</span>
+                            millons de dollars à dépenser selon quelques règles.</p>
+                        <p class="card-text mb-1">Tu choisis <span class="font-weight-bold tertiary">12</span>
+                            joueurs pour ton roster, pas un de plus, pas un de moins !
+                        Il te faut 5 arrières, 5 ailiers, et 2 pivots.</p>
+                        <p class="card-text mb-1">Tu as <span class="font-weight-bold tertiary">48</span>
+                            heures pour boucler ta draft, et <span class="font-weight-bold tertiary">6</span> heures
+                            pour renchérir sur une enchère existante sur un joueur.</p>
+                        <p class="card-text mb-1">Passé le délai de fin de draft, tu ne peux plus enchérir sur aucun joueur.
+                         Si tu n'as pas les 12 joueurs requis pour jouer, les joueurs manquant à ta team seront ajoutés
+                        aléatoirement à ta team parmi ceux qui ne coûtent rien.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{--        Quatrième section tuto : matchs --}}
+        <div class="container my-5">
+            <div class="row py-5">
+                <div class="col-md-6 card shadow-lg">
+                    <div class="py-4 d-flex flex-column align-items-center">
+                        <h1 class="display-2 text-dark">4th Step</h1>
+                        <img src="{{asset('storage/images/picto_matchs.png')}}" alt="photo">
+                        <h2>Fais ton 5 majeur et affronte tes adversaires en match</h2>
+                    </div>
+                    <div class="card-body mt-1">
+                        <p class="card-text mb-1">Cette étape est cruciale !</p>
+                        <p class="card-text mb-1">Tu dois choisir <span class="font-weight-bold tertiary">1</span> pivot,
+                            <span class="font-weight-bold tertiary">2</span> ailiers et
+                            <span class="font-weight-bold tertiary">2</span> arrières pour ton match.</p>
+                        <p class="card-text mb-1">Choisis bien car tes choix seront définitifs
+                            <span class="font-weight-bold tertiary">4</span> jours avant
+                            le début de la nouvelle journée NBA.</p>
+                        <p class="card-text mb-1">Comme tu t'en doutes, la victoire de ton équipe va dépendre des performances
+                            en NBA des joueurs de ton roster.</p>
+                        <p class="card-text mb-1">Si la somme des scores individuels de tes joueurs dépasse celle de ton adversaire,
+                             tu gagnes.</p>
+                        <p class="card-text mb-1">Les informations de match, l'accès à toutes les fonctionnalités
+                            se fera depuis ton tableau de bord.</p>
+                    </div>
+                </div>
+                <div class="col-md-6" id="fourth-step"></div>
+            </div>
+            <div class="row">
+                <div class="col-12 text-center"><a href="{{'leagues'}} " class="btn btn-secondary">Let's go !</a></div>
+            </div>
+        </div>
+
+    </div>
+
 @endsection
